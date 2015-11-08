2d54fdfa98b6946386812c817c76c477ee7845e9
Eric Typaldos <ettypaldos@gmail.com>
2015-11-08 13:33:21 -0800

    Bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'typ:accounts-huddle',
   summary: 'OAuth2 integration with Huddle',
-  version: '0.0.1',
+  version: '0.0.2',
   documentation: './README.md',
   git: 'https://github.com/typ90/meteor-accounts-huddle'
 });
